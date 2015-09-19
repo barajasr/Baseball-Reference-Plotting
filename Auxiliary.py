046ad6c41802efed139734507e76b1ad6c9063fe
barajasr <barajsar89@gmail.com>
2015-09-19 04:59:46 -0700

    Completion of both mechanisms to gather and plot data regarding win/loss wrt runs scored and conceded. Changes outside of scope of above is moving of namedtuples to Auxilary.py and usage for [hopefully] improved readability. Additionally using OPTIONS to switch Plot.x_min appropiately.

--- Auxiliary.py
+++ Auxiliary.py
@@ -1,8 +1,12 @@
-""" Authour: Richard Barajas
-    Date: Sept. 15, 2015
-"""
+from collections import namedtuple
+
 TARGETS = ['scores', 'wins_losses']
 LOSS, WIN = 'L', 'W'
+
+Axis = namedtuple('Axis', 'min max')
+Data = namedtuple('Data', 'negative positive')
+Labels = namedtuple('Labels', 'team x y tag')
+Record = namedtuple('Record', 'wins, losses')
 
 def count_wins_losses(raw_data):
     """ From incoming list of ['W', 'L', ...] record win/loss streaks.
@@ -22,6 +26,44 @@ def count_wins_losses(raw_data):
     # Active streak
     update_dicts(streaks_set, previous, streak)
     return streaks_set
+
+def outcome_record(data, histogram):
+    """ Return win/loss record of team when using outcome_[conceding|scoring]
+        plots. The zero index adds quirk not found in others.
+    """
+    losses, wins = 0, 0
+    if histogram:
+        return Record(sum(data.positive),  -sum(data.negative))
+    else:
+        for index in range(len(data[0])):
+            if index > 0:
+                wins += data.positive[index] // index
+                losses += -data.negative[index] // index
+            else:
+                wins += data.positive[index]
+                losses += -data.negative[index]
+
+    return Record(wins, losses)
+
+def outcome_when_conceding(raw_data):
+    """ From incoming list of [[RF, RA], ...] record win/loss wrt RA.
+    """
+    outcomes_set = {LOSS : {}, WIN : {}}
+    for runs_for, runs_against in raw_data:
+        outcome = WIN if runs_for - runs_against > 0 else LOSS
+        update_dicts(outcomes_set, outcome, runs_against)
+
+    return outcomes_set
+
+def outcome_when_scoring(raw_data):
+    """ From incoming list of [[RF, RA], ...] record win/loss relative wrt RF.
+    """
+    outcomes_set = {LOSS : {}, WIN : {}}
+    for runs_for, runs_against in raw_data:
+        outcome = WIN if runs_for - runs_against > 0 else LOSS
+        update_dicts(outcomes_set, outcome, runs_for)
+
+    return outcomes_set
 
 def update_dicts(dicts, key, subkey):
     """ Increment or insert subkey relating dict with key as top level dict key.
--- Plot.py
+++ Plot.py
@@ -1,5 +1,3 @@
-from collections import namedtuple
-
 import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -8,8 +6,6 @@ import numpy as np
 import Auxiliary as aux
 import BrefScraper as brf
 
-Labels = namedtuple('Labels', 'team x y tag')
-Axis = namedtuple('Axis', 'min max')
 
 class Plot(object):
     """ With data obtained from BrefScraper, Plot clean the raw
@@ -40,32 +36,34 @@ class Plot(object):
         negative, positive = [], []
         bi_keys = aux.LOSS, aux.WIN
 
-        for index in range(1, x_max+1):
+        for index in range(self.x_min, x_max+1):
             negative.append(0)
             positive.append(0)
             if index in data[bi_keys[0]]:
                 negative[-1] = -data[bi_keys[0]][index]
                 if not self.histogram:
-                    negative[-1] *= index
+                    if index != 0 or self.x_min != 0:
+                        negative[-1] *= index
 
             if index in data[bi_keys[1]]:
                 positive[-1] = data[bi_keys[1]][index]
                 if not self.histogram:
-                    positive[-1] *= index
+                    if index != 0 or self.x_min != 0:
+                        positive[-1] *= index
 
-        return negative, positive
+        return aux.Data(negative, positive)
 
     def _fit_y_axis(self, data):
         """ Adjust Y-axis range to next minor tick if required.
         """
         y_min, y_max = self.y_min, self.y_max
-        set_min = min(data[0])
+        set_min = min(data.negative)
         if set_min <= self.y_min:
             y_min = set_min - (self.minor - set_min % self.minor)
-        set_max = max(data[1])
+        set_max = max(data.positive)
         if set_max >= self.y_max:
             y_max = set_max + (self.minor - set_max % self.minor)
-        return Axis(y_min, y_max)
+        return aux.Axis(y_min, y_max)
 
     def _max_key(self, data):
         """ Return the max x-axis value found in keys.
@@ -77,41 +75,70 @@ class Plot(object):
             key_max = dict_max
         return key_max
 
-    def set_default_axes(self, x_min=1, x_max=10, y_min=-24, y_max=24):
-        """ Adjust default axes range.
-        """
-        self.x_min, self.x_max = x_min, x_max
-        self.y_min, self.y_max = y_min, y_max
 
     def plot(self, plot_type):
         """ Main point of entry. Set off scraper, process and plot data.
         """
         # Dict with appropiate functions for data transforming defined
-        # at bottom of module
-        (teams_raw, team_set, get_clean, to_plot) = OPTIONS[plot_type]
+        # at bottom of module.
+        (self.x_min, teams_raw, team_set, get_clean, to_plot) = OPTIONS[plot_type]
         for team, raw_data in teams_raw(self.scraper):
             raw_set = team_set(raw_data)
             data = get_clean(self, raw_set)
             to_plot(self, team, data)
 
+    def _plot_outcome_conceding(self, team, data):
+        """ Sets the specific params for of win/loss outcome when team concedes
+            x runs.
+        """
+        y_axis = self._fit_y_axis(data)
+        record = aux.outcome_record(data, self.histogram)
+
+        y_label = 'Wins/losses when conceding x runs' if self.histogram else\
+                  'Total runs sorted by runs conceded per game'
+        tag_label = 'outcome_conceding_histogram' if self.histogram else\
+                    'outcome_conceding_sorted'
+
+        self._plot_team(data,
+                        record,
+                        aux.Labels(team, 'Runs conceded', y_label, tag_label),
+                        y_axis)
+
+    def _plot_outcome_scoring(self, team, data):
+        """ Sets the specific params for of win/loss outcome when team scores
+            x runs.
+        """
+        y_axis = self._fit_y_axis(data)
+        record = aux.outcome_record(data, self.histogram)
+
+        y_label = 'Wins/losses when scoring x runs' if self.histogram else\
+                  'Total runs sorted by runs scored per game'
+        tag_label = 'outcome_scoring_histogram' if self.histogram else\
+                    'outcome_scoring_sorted'
+
+        self._plot_team(data,
+                        record,
+                        aux.Labels(team, 'Runs scored', y_label, tag_label),
+                        y_axis)
+
     def _plot_team(self, data, record, labels, y_axis):
         """ Generic plotting for data found on the team's schedule and results
             page.
         """
-        net = [n + m for m, n in zip(data[0], data[1])]
+        net = [n + m for m, n in zip(data.negative, data.positive)]
 
         fig = plt.figure()
         plt.xlabel(labels.x)
         plt.ylabel(labels.y)
         # record turned into string for int/float possibilty
-        if isinstance(record[0], int):
+        if isinstance(record.wins, int):
             plt.title('{} ({}-{}) - {}'\
-                .format(labels.team, record[0], record[1], self.scraper.year))
+               .format(labels.team, record.wins, record.losses, self.scraper.year))
         else:
             plt.title('{} ({:.2f}-{:.2f}) - {}'\
-               .format(labels.team, record[0], record[1], self.scraper.year))
+               .format(labels.team, record.wins, record.losses, self.scraper.year))
 
-        x_max = len(data[0]) + 1
+        x_max = len(data.negative) + 1 if self.x_min == 1 else len(data.negative)
         plt.axis([self.x_min, x_max, y_axis.min, y_axis.max])
         ax = plt.subplot()
         ax.set_xticks(np.arange(1, x_max, 1))
@@ -122,9 +149,9 @@ class Plot(object):
         ax.yaxis.set_major_formatter(major_formatter)
         ax.yaxis.set_minor_locator(minor_locator)
 
-        x_axis = range(1, x_max)
-        ax.bar(x_axis, data[0], width=0.96, color='r', edgecolor=None, linewidth=0)
-        ax.bar(x_axis, data[1], width=0.96, color='b', edgecolor=None, linewidth=0)
+        x_axis = range(self.x_min, x_max)
+        ax.bar(x_axis, data.negative, width=0.96, color='r', edgecolor=None, linewidth=0)
+        ax.bar(x_axis, data.positive, width=0.96, color='b', edgecolor=None, linewidth=0)
         ax.bar(x_axis, net, width=0.96, color='g', edgecolor=None, linewidth=0, label='Net')
         plt.axhline(0, color='black')
 
@@ -142,37 +169,37 @@ class Plot(object):
         """ Sets the specific params for margins of win/loss plot.
         """
         y_axis = self._fit_y_axis(data)
-        record = [0, 0]
-        record[0] = sum(data[1]) if self.histogram else\
-                    sum([runs // (margin + 1) for margin, runs in enumerate(data[1])])
-        record[1] = -sum(data[0]) if self.histogram else\
-                    -sum([runs // (margin + 1) for margin, runs in enumerate(data[0])])
+        wins = sum(data.positive) if self.histogram else\
+               sum([runs // (margin + 1) \
+               for margin, runs in enumerate(data.positive)])
+        losses = -sum(data.negative) if self.histogram else\
+                 -sum([runs // (margin + 1) \
+                 for margin, runs in enumerate(data.negative)])
 
         y_label = '# of times won/loss by margin' if self.histogram else\
                   'Total Runs sorted by margin'
         tag_label = 'margin_histogram' if self.histogram else 'margin_sorted'
 
         self._plot_team(data,
-                        record,
-                        Labels(team, 'Margin of win/loss', y_label, tag_label),
+                        aux.Record(wins, losses),
+                        aux.Labels(team, 'Margin of win/loss', y_label, tag_label),
                         y_axis)
 
     def _plot_win_loss_streaks(self, team, data):
         """ Sets the specific params for win/loss streaks plot.
         """
         y_axis = self._fit_y_axis(data)
-        record = [0, 0]
-        record[0] = sum([(m + 1) * n for m, n in enumerate(data[1])]) \
-                    if self.histogram else sum(data[1])
-        record[1] = -sum([(m + 1) * n for m, n in enumerate(data[0])]) \
-                    if self.histogram else -sum(data[0])
+        wins = sum([(m + 1) * n for m, n in enumerate(data.positive)]) \
+               if self.histogram else sum(data.positive)
+        losses = -sum([(m + 1) * n for m, n in enumerate(data.negative)]) \
+                 if self.histogram else -sum(data.negative)
 
         y_label = '# of Streaks' if self.histogram else 'Win/Losses sorted by streak'
         tag_label = 'streaks_histogram' if self.histogram else 'streaks_sorted'
 
         self._plot_team(data,
-                        record,
-                        Labels(team, 'Streak Length', y_label, tag_label),
+                        aux.Record(wins, losses),
+                        aux.Labels(team, 'Streak Length', y_label, tag_label),
                         y_axis)
 
     def _save(self, filename, directory, ext='png', close=True, verbose=True):
@@ -191,12 +218,30 @@ class Plot(object):
         if close:
             plt.close()
 
+    def set_default_axes(self, x_min=1, x_max=10, y_min=-24, y_max=24):
+        """ Adjust default axes range.
+        """
+        self.x_min, self.x_max = x_min, x_max
+        self.y_min, self.y_max = y_min, y_max
+
 # Data transformation and plotting chain
-OPTIONS = {'win_loss_streaks' : [brf.BrefScraper.wins_losses,
+OPTIONS = {'outcome_conceding': [0,
+                                 brf.BrefScraper.game_scores,
+                                 aux.outcome_when_conceding,
+                                 Plot._clean_data,
+                                 Plot._plot_outcome_conceding],
+           'outcome_scoring': [0,
+                               brf.BrefScraper.game_scores,
+                               aux.outcome_when_scoring,
+                               Plot._clean_data,
+                               Plot._plot_outcome_scoring],
+           'win_loss_streaks' : [1,
+                                 brf.BrefScraper.wins_losses,
                                  aux.count_wins_losses,
                                  Plot._clean_data,
                                  Plot._plot_win_loss_streaks],
-           'win_loss_margins' : [brf.BrefScraper.game_scores,
+           'win_loss_margins' : [1,
+                                 brf.BrefScraper.game_scores,
                                  aux.win_loss_margins,
                                  Plot._clean_data,
                                  Plot._plot_win_loss_margins]}
